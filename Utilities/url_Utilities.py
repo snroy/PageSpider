321f8c9bd94324c021f4fe3a2ccb546fa9fb9480
snroy <42188585+snroy@users.noreply.github.com>
2018-11-29 14:47:20 -0500

    Update url_Utilities.py

--- Utilities/url_Utilities.py
+++ Utilities/url_Utilities.py
@@ -2,9 +2,11 @@ def load_urls_from_file(file_path: str):
     #TODO: add code to read file containing url
     pass
 
+
 def load_page(url: str):
     #TODO: add code to read url
     pass
+
 
 def scrape_page(page_contents: str):
     #TODO: analyze text
