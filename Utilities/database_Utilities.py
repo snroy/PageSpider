86ca89a06f858215a632d425ff54edbbeb124d09
snroy <42188585+snroy@users.noreply.github.com>
2018-11-29 14:47:48 -0500

    Update database_Utilities.py

--- Utilities/database_Utilities.py
+++ Utilities/database_Utilities.py
@@ -2,6 +2,7 @@ def create_database(database_path: str):
     #TODO: code to create database
     pass
 
+
 def read_words_from_database(database_path: str, word_list: list):
     #TODO: save words to database
     pass
